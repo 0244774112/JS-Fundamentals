e0c62a4f41ec2f0e91da63bf4efe26378182b19e
Sandy Owusu Bremang <sandyowusubremang@gmail.com>
2025-07-13 22:17:22 +0500

    added cnages

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,9 @@
 const args = process.argv.slice(2);
 const times = parseInt(args[0]);
-if (isNaN(times) || times <= 0) {
+if (isNaN(times)) {
   console.log("Missing number of occurrences");
 } else {
-  let output = "";
   for (let i = 0; i < times; i++) {
-    output += "C is fun\n";
+    console.log("C is fun");
   }
-  console.log(output.trim());
 }
